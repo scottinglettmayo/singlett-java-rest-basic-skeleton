04ec65fc9250394b7b117ffe871a06431b81fbd8
Scott Inglett <inglett.scott@mayo.edu>
2019-07-18 10:24:32 -0500

    Add infrastructure as code

---
+++ infra/index.ts
@@ -0,0 +1,24 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as gcp from "@pulumi/gcp";
+
+const app = new gcp.appengine.Application("app", {
+    locationId: "us-central",
+    project: pulumi.getProject()
+});
+
+const rule = new gcp.appengine.FirewallRule("rule", {
+    action: "ALLOW",
+    priority: 1000,
+    project: app.project,
+    sourceRange: "*",
+});
+
+const filename_trigger = new gcp.cloudbuild.Trigger("filename-trigger", {
+    filename: "GCP/cloudbuild.yaml",
+    substitutions: {
+    },
+    triggerTemplate: {
+        branchName: "master",
+        repoName: "github_scottinglettmayo_singlett-java-rest-basic-skeleton",
+    },
+});
